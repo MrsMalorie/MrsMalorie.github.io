05cfab943279f25c04fa52abceba76196cd1677e
Quack Pot <skyler32702@gmail.com>
2026-08-01 18:24:31 -0500

    404 Page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,19 +1,14 @@
-import { Title } from "@solidjs/meta";
-import { HttpStatusCode } from "@solidjs/start";
-
 export default function NotFound() {
   return (
-    <main>
-      <Title>Not Found</Title>
-      <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+    <main class="bg-gradient-to-b from-blue-300 to-white w-screen h-screen flex items-center justify-center">
+        <div class="bg-white/80 rounded-lg p-8 shadow-lg">
+            <h1 class="font-[Bitter] font-bold text-xl">
+                404 - Page Not Found
+            </h1>
+            <p class="mt-2 text-gray-700">
+                The link provided did not resolve to a valid webpage. Please return <a href="/" class="text-blue-700 hover:underline">home</a>.
+            </p>
+        </div>
     </main>
   );
 }
