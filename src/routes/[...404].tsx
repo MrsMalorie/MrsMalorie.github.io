e74328b02d90b58fd6d56543381e65d86c74ebaa
Quack Pot <skyler32702@gmail.com>
2026-08-01 18:45:01 -0500

    404 Bros

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,13 +1,15 @@
 export default function NotFound() {
   return (
-    <main class="bg-gradient-to-b from-blue-300 to-white w-screen h-screen flex items-center justify-center">
-        <div class="bg-white/80 rounded-lg p-8 shadow-lg">
+    <main class="bg-gradient-to-b from-blue-300 to-white w-screen h-screen flex flex-col items-center justify-center">
+        <div class="bg-white rounded-lg p-8 shadow-lg z-5 relative">
             <h1 class="font-[Bitter] font-bold text-xl">
                 404 - Page Not Found
             </h1>
             <p class="mt-2 text-gray-700">
-                The link provided did not resolve to a valid webpage. Please return <a href="/" class="text-blue-700 hover:underline">home</a>.
+                Clearly, you aren't supposed to be here... Just go <a href="/" class="text-blue-700 hover:underline">home</a> already.
             </p>
+
+            <img src="/assets/404_bros.webp" class="absolute top-0 left-[50%] translate-x-[-50%] translate-y-[-100%] w-80" />
         </div>
     </main>
   );
