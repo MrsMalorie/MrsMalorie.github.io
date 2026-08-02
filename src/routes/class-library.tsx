9a1d6a52e21850a026f826c821d7bc6d9b6254c6
Quack Pot <skyler32702@gmail.com>
2026-08-02 12:09:15 -0500

    Book table rows

--- src/routes/class-library.tsx
+++ src/routes/class-library.tsx
@@ -5,6 +5,8 @@ import BookController from "~/lib/controllers/BookController";
 import Book from "~/lib/types/Book";
 import ChevronLeft from "lucide-solid/icons/chevron-left";
 import ChevronRight from "lucide-solid/icons/chevron-right";
+import BadgeCheck from "lucide-solid/icons/badge-check";
+import BadgeX from "lucide-solid/icons/badge-x";
 import { count } from "~/lib/utils/array";
 
 const NULL_GENRE: string = "N/A";
@@ -16,6 +18,78 @@ const LOWEST_AR_POINTS: number = 0.0;
 const HIGHEST_AR_POINTS: number = 170; // Highest Point Value Computed Using World-Record Highest Word Count (August 2025)
 
 const PAGE_SIZE: number = 50;
+
+function BookTableRow(book: Book, index: number) {
+    const has_ar = book.ar_data != undefined;
+    const points = has_ar ? book.ar_data?.points : "N/A";
+    const quiz_id = has_ar ? book.ar_data?.quiz_id : "N/A";
+
+    return (<>
+        <Show when={index > 0}>
+            <tr>
+                <td class="h-4"></td>
+            </tr>
+            <tr class="border-t-4 border-dashed border-[#d7a350]">
+                <td class="h-4"></td>
+            </tr>
+        </Show>
+
+        <tr class="hidden md:table-row">
+            <td class="flex items-center">{has_ar
+                ? <BadgeCheck class="w-4 h-4 text-green-500" />
+                : <BadgeX class="w-4 h-4 text-red-500" />
+            }&nbsp;AR</td>
+            <td>{book.genres}</td>
+            <td>Level:&nbsp;{book.reading_level}</td>
+            <td>AR Points:&nbsp;{points}</td>
+            {quiz_id === "N/A"
+                ? <td>Quiz:&nbsp;{quiz_id}</td>
+                : <td>Quiz:&nbsp;
+                    <a
+                        href={`https://www.arbookfind.com/bookdetail.aspx?q=${quiz_id}&l=EN`}
+                        class="pointer-cursor text-blue-500 hover:underline"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >{quiz_id}</a>
+                </td>
+            }
+        </tr>
+
+        <tr class="md:hidden">
+            <td class="flex items-center" colSpan={2}>{has_ar
+                ? <BadgeCheck class="w-4 h-4 text-green-500" />
+                : <BadgeX class="w-4 h-4 text-red-500" />
+            }&nbsp;AR</td>
+            <td colSpan={3} class="text-right">{book.genres}</td>
+        </tr>
+
+        <tr class="md:hidden">
+            <td colSpan={2}>Level:&nbsp;{book.reading_level}</td>
+            <td colSpan={3} class="text-right">AR Points:&nbsp;{points}</td>
+        </tr>
+
+        <tr class="md:hidden">
+            {quiz_id === "N/A"
+                ? <td colSpan={5}>Quiz:&nbsp;{quiz_id}</td>
+                : <td colSpan={5}>Quiz:&nbsp;
+                    <a
+                        href={`https://www.arbookfind.com/bookdetail.aspx?q=${quiz_id}&l=EN`}
+                        class="pointer-cursor text-blue-500 hover:underline"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >{quiz_id}</a>
+                </td>
+            }
+        </tr>
+
+        <tr>
+            <td colSpan={5} class="font-bold">{book.title}</td>
+        </tr>
+        <tr>
+            <td colSpan={5} class="font-bold">By:&nbsp;{book.author}</td>
+        </tr>
+    </>);
+}
 
 export default function ClassLibraryPage() {
     const [loadingGenres, setLoadingGenres] = createSignal<boolean>(true);
@@ -74,7 +148,7 @@ export default function ClassLibraryPage() {
                 <Title>Class Library</Title>
             </MetaProvider>
 
-            <div class="min-h-screen striped-background border-x-20 border-x-[#d7a350] bg-blend-overlay bg-white/80 px-4 sm:px-8 py-16 flex flex-col gap-4">
+            <div class="min-h-screen striped-background border-x-20 border-x-[#d7a350] bg-blend-overlay bg-white/90 px-4 sm:px-8 py-16 flex flex-col gap-4">
                 <div class="space-y-2">
                     <h1 class="font-bold text-3xl">Search the Class Library</h1>
                     <hr />
@@ -88,7 +162,7 @@ export default function ClassLibraryPage() {
                     <Show when={loadingGenres() || loadingBooks()} fallback={<>
                         <table class="w-full">
                             <tbody>
-
+                                {books().map(BookTableRow)}
                             </tbody>
                         </table>
 
